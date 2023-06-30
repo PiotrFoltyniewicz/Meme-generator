0003065eb1e9c5727a25a3d899d45a3de5939d87
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2023-06-30 14:51:33 +0200

    Shows meme image from file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import icon from '../assets/logo.svg'
 export default function Header(){
     return(
         <header className='header'>
-            <div class='header--leftWrapper'>
+            <div className='header--leftWrapper'>
                 <img src={icon} className='header--icon'/>
                 <h2 className='header--title'>Meme Generator</h2>
             </div>
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,24 @@
+import memesData from '../memesData'
+import {useState} from 'react';
+
 export default function Meme(){
+
+    const [memeUrl, setMemeUrl] = useState('');
+
+    function getMeme(){
+        const memesArray = memesData.data.memes;
+        const url = memesArray[Math.floor(Math.random() * memesArray.length)].url;
+        setMemeUrl(url);
+    }
+
     return (
-        <form className='meme' method='get'>
+        <div className='meme' method='get'>
             <div className="meme--inputWrapper">
                 <input className='meme--input' type='text' placeholder='Upper Text'/>
                 <input className='meme--input' type='text' placeholder='Bottom Text'/>
             </div>
-            <button className='meme--button'>Get a new meme image</button>
-        </form>
+            <button className='meme--button' onClick={getMeme}>Get a new meme image</button>
+            <img src={memeUrl} className='meme--imageOut'/>
+        </div>
     )
 }
